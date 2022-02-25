83a5465b09192fbddcc9e72a1f08999442c508e4
PragmaticBeaver <dominik.blatzheim@mailbox.org>
2022-02-25 16:21:29 +0100

    refactor: WIP extract known paragraphs

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,191 @@
 // For action titles, the first word has to start with a capital letter, followed by 0-3 other words, ignoring prepositions,
 // followed by a period. Support words with hyphens, non-capital first letter, and parentheses like '(Recharge 5-6)'.
-const actionTitleRegex =
-  /^(([A-Z]\w+[ \-]?)(\s(of|and|the|from|in|at|on|with|to|by)\s)?(\w+ ?){0,3}(\([\w –\-\/]+\))?)\./;
-const racialDetailsRegex =
-  /^(?<size>\bfine\b|\bdiminutive\b|\btiny\b|\bsmall\b|\bmedium\b|\blarge\b|\bhuge\b|\bgargantuan\b|\bcolossal\b)\s(?<type>\w+)([,|\s]+\((?<race>[\w|\s]+)\))?([,|\s]+(?<alignment>[\w|\s]+))?/i;
-const armorRegex =
-  /^((armor|armour) class) (?<ac>\d+)( \((?<armortype>.+)\))?/i;
-const healthRegex =
-  /^(hit points) (?<hp>\d+) \((?<formula>\d+d\d+( ?[\+|\-|−|–] ?\d+)?)\)/i;
-const speedRegex = /(?<name>\w+) (?<value>\d+)/gi;
-const abilityNamesRegex = /\bstr\b|\bdex\b|\bcon\b|\bint\b|\bwis\b|\bcha\b/gi;
-const abilitySavesRegex =
-  /(?<name>\bstr\b|\bdex\b|\bcon\b|\bint\b|\bwis\b|\bcha\b) (?<modifier>[\+|-]\d+)/gi;
-const abilityValuesRegex = /(?<base>\d+)\s?\((?<modifier>[\+|\-|−|–]\d+)\)/g;
-const skillsRegex =
-  /(?<name>\bacrobatics\b|\barcana\b|\banimal handling\b|\bathletics\b|\bdeception\b|\bhistory\b|\binsight\b|\bintimidation\b|\binvestigation\b|\bmedicine\b|\bnature\b|\bperception\b|\bperformance\b|\bpersuasion\b|\breligion\b|\bsleight of hand\b|\bstealth\b|\bsurvival\b) (?<modifier>[\+|-]\d+)/gi;
-const damageTypesRegex =
-  /\bbludgeoning\b|\bpiercing\b|\bslashing\b|\bacid\b|\bcold\b|\bfire\b |\blightning\b|\bnecrotic\b|\bpoison\b|\bpsychic\b|\bradiant\b|\bthunder\b|/gi;
-const sensesRegex =
-  /(?<name>\bdarkvision\b|\bblindsight\b|\btremorsense\b|\btruesight\b) (?<modifier>\d+)/i;
-const challengeRegex = /^challenge (?<cr>(½|[\d/]+)) \((?<xp>[\d,]+)/i;
-const spellCastingRegex =
-  /\((?<slots>\d+) slot|(?<perday>\d+)\/day|spellcasting ability is (?<ability>\w+)|spell save dc (?<savedc>\d+)/gi;
-const spellLevelRegex = /(?<level>\d+)(.+)level spellcaster/i;
-const attackRegex = /(attack|damage): \+(?<tohit>\d+) to hit/i;
-const reachRegex = /reach (?<reach>\d+) ?(ft|'|’)/i;
-const rangeRegex = /range (?<near>\d+)\/(?<far>\d+) ?(ft|'|’)/i;
-const rechargeRegex = /\(recharge (?<recharge>\d+)([–|-]\d+)?\)/i;
-const savingThrowRegex = /dc (?<savedc>\d+) (?<saveability>\w+) saving throw/i;
-const versatileRegex =
-  /\((?<damageroll>\d+d\d+( ?\+ ?\d+)?)\) (?<damagetype>\w+) damage if used with two hands/i;
-const targetRegex = /(?<range>\d+)?-(foot|ft?.|'|’) (?<shape>\w+)/i;
-const damageRollsQuery =
-  "(?<={0})[\\s\\w\\d,]+\\((?<damageroll1>\\d+d\\d+)( \\+ (?<damagemod1>\\d+))?\\) (?<damagetype1>\\w+)(.+plus.+\\((?<damageroll2>\\d+d\\d+( \\+ (?<damagemod2>\\d+))?)\\) (?<damagetype2>\\w+))?";
 
-async function createActorSheet(actorData) {
-  // actorData = actorData.replace(/\n/g, "");
-  // console.log(actorData);
+// const actionTitleRegex =
+//   /^(([A-Z]\w+[ \-]?)(\s(of|and|the|from|in|at|on|with|to|by)\s)?(\w+ ?){0,3}(\([\w –\-\/]+\))?)\./;
+// const racialDetailsRegex =
+//   /^(?<size>\bfine\b|\bdiminutive\b|\btiny\b|\bsmall\b|\bmedium\b|\blarge\b|\bhuge\b|\bgargantuan\b|\bcolossal\b)\s(?<type>\w+)([,|\s]+\((?<race>[\w|\s]+)\))?([,|\s]+(?<alignment>[\w|\s]+))?/i;
+// const armorRegex =
+//   /^((armor|armour) class) (?<ac>\d+)( \((?<armortype>.+)\))?/i;
+// const healthRegex =
+//   /^(hit points) (?<hp>\d+) \((?<formula>\d+d\d+( ?[\+|\-|−|–] ?\d+)?)\)/i;
+// const speedRegex = /(?<name>\w+) (?<value>\d+)/gi;
+// const abilityNamesRegex = /\bstr\b|\bdex\b|\bcon\b|\bint\b|\bwis\b|\bcha\b/gi;
+// const abilitySavesRegex =
+//   /(?<name>\bstr\b|\bdex\b|\bcon\b|\bint\b|\bwis\b|\bcha\b) (?<modifier>[\+|-]\d+)/gi;
+// const abilityValuesRegex = /(?<base>\d+)\s?\((?<modifier>[\+|\-|−|–]\d+)\)/g;
+// const skillsRegex =
+//   /(?<name>\bacrobatics\b|\barcana\b|\banimal handling\b|\bathletics\b|\bdeception\b|\bhistory\b|\binsight\b|\bintimidation\b|\binvestigation\b|\bmedicine\b|\bnature\b|\bperception\b|\bperformance\b|\bpersuasion\b|\breligion\b|\bsleight of hand\b|\bstealth\b|\bsurvival\b) (?<modifier>[\+|-]\d+)/gi;
+// const damageTypesRegex =
+//   /\bbludgeoning\b|\bpiercing\b|\bslashing\b|\bacid\b|\bcold\b|\bfire\b |\blightning\b|\bnecrotic\b|\bpoison\b|\bpsychic\b|\bradiant\b|\bthunder\b|/gi;
+// const sensesRegex =
+//   /(?<name>\bdarkvision\b|\bblindsight\b|\btremorsense\b|\btruesight\b) (?<modifier>\d+)/i;
+// const challengeRegex = /^challenge (?<cr>(½|[\d/]+)) \((?<xp>[\d,]+)/i;
+// const spellCastingRegex =
+//   /\((?<slots>\d+) slot|(?<perday>\d+)\/day|spellcasting ability is (?<ability>\w+)|spell save dc (?<savedc>\d+)/gi;
+// const spellLevelRegex = /(?<level>\d+)(.+)level spellcaster/i;
+// const attackRegex = /(attack|damage): \+(?<tohit>\d+) to hit/i;
+// const reachRegex = /reach (?<reach>\d+) ?(ft|'|’)/i;
+// const rangeRegex = /range (?<near>\d+)\/(?<far>\d+) ?(ft|'|’)/i;
+// const rechargeRegex = /\(recharge (?<recharge>\d+)([–|-]\d+)?\)/i;
+// const savingThrowRegex = /dc (?<savedc>\d+) (?<saveability>\w+) saving throw/i;
+// const versatileRegex =
+//   /\((?<damageroll>\d+d\d+( ?\+ ?\d+)?)\) (?<damagetype>\w+) damage if used with two hands/i;
+// const targetRegex = /(?<range>\d+)?-(foot|ft?.|'|’) (?<shape>\w+)/i;
+// const damageRollsQuery =
+// "(?<={0})[\\s\\w\\d,]+\\((?<damageroll1>\\d+d\\d+)( \\+ (?<damagemod1>\\d+))?\\) (?<damagetype1>\\w+)(.+plus.+\\((?<damageroll2>\\d+d\\d+( \\+ (?<damagemod2>\\d+))?)\\) (?<damagetype2>\\w+))?";
 
-  var lines = actorData.trim().split(/\n/g);
-  for (var i = 0; i < lines.length; i++) {
-    // todo
-    const line = lines[i].trim();
-    console.log([i] + ". ", line);
+const KNOWN_SECTION_HEADERS = [
+  "actions",
+  "bonus actions",
+  "reactions",
+  "legendary actions",
+  "lair actions",
+  "regional effects",
+];
+
+const SECTION_IDS = {
+  0: "coreData",
+  1: "coreAttributes",
+  2: "abilities",
+  3: "mixedSection",
+  4: "legendaryResistances",
+  5: "features",
+  6: "actions",
+  7: "legendaryActions",
+};
+
+function findParagraphEnd(lines, statIdx) {
+  for (let j = statIdx + 1; j < lines.length; j++) {
+    const pLine = lines[j];
+    if (
+      KNOWN_SECTION_HEADERS.includes(pLine.toLowerCase()) ||
+      j === lines.length
+    ) {
+      const endIndex = j === lines.length ? j : j - 1;
+      return endIndex;
+    }
   }
+}
+
+function findParagraphIndexes(lines) {
+  const pIndices = { start: undefined, end: undefined };
+
+  for (let i = 0; i <= lines.length; i++) {
+    const line = lines[i].trim();
+
+    const isNotSectionHeader =
+      !KNOWN_SECTION_HEADERS.includes(line.toLowerCase()) ||
+      line === "" ||
+      line === undefined;
+    if (isNotSectionHeader) {
+      continue;
+    }
+
+    pIndices.start = i;
+    pIndices.end = findParagraphEnd(lines, i);
+    return pIndices;
+  }
+}
+
+function extractParagrah(lines, paragraphIndices) {
+  const start = paragraphIndices.start;
+  const end = paragraphIndices.end;
+
+  const paragraph = lines.slice(start, end + 1);
+  console.log("paragraph", paragraph);
+
+  const length = end - start;
+  lines.splice(start, length);
+  console.log("remaining lines", lines);
+
+  return paragraph;
+}
+
+function findParagraphs(lines) {
+  // const paragraphs = [];
+
+  /**
+   * Paragraph
+   * + header
+   * + value
+   */
+  while (true) {
+    const pIndices = findParagraphIndexes(lines);
+    if (!pIndices.end) {
+      console.log("no paragraphs left");
+      break;
+    }
+    const paragraph = extractParagrah(lines, pIndices);
+    // todo save paragraph
+  }
+  console.log("ending paragraph discovery");
+}
+
+function collectActionData(lines, sections) {
+  console.log(lines);
+  /**
+   * todo
+   * check for presents of known section headers (actions, bonus actions, reactions, legendary actions, lair actions, regional effects)
+   * remove these sections from array
+   * remaining can be read line by line
+   * but section end will be end of array or empty line
+   */
+
+  /**
+   * todo
+   * 1. collect header start and end by index
+   * 2. add lines into sections
+   * 3. remove this part of the array
+   */
+
+  // todo
+  const p = findParagraphs(lines);
+  console.log("p", p);
+}
+
+function collectRemainingActorData(lines, sections) {
+  let sectionIndex = 0;
+  for (let i = 0; i <= lines.length; i++) {
+    const line = lines[i].trim();
+    console.log("lines.length", lines.length);
+    console.log("line " + i, line);
+
+    const newSection = line.length <= 0 && i != lines.length;
+    if (newSection) {
+      sectionIndex += 1;
+    }
+    console.log("sectionIds", SECTION_IDS);
+    console.log("sectionIndex", sectionIndex);
+
+    const sectionKey = SECTION_IDS[sectionIndex] ?? "other";
+    console.log("sectionKey", sectionKey);
+    console.log("sections", sections);
+
+    sections[sectionKey].push(line);
+  }
+}
+
+function createActorSheet(actorData) {
+  const sections = {
+    coreData: [],
+    coreAttributes: [],
+    abilities: [],
+    mixedSection: [],
+    legendaryResistances: [],
+    features: [],
+    actions: [],
+    legendaryActions: [],
+    other: [],
+  };
+
+  const lines = actorData.trim().split(/\n/g);
+  collectActionData(lines, sections);
+  // collectRemainingActorData(lines, sections);
+
+  // for (const [key, val] of sections) {
+  //   console.log(key, ": ", val);
+  // }
 
   /**
    * OTHER VALUES TO BE SET
