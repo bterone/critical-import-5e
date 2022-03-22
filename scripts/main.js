55eddab3e4a34ebb122d185288d4c82c4cfce1cc
PragmaticBeaver <dominik.blatzheim@mailbox.org>
2022-03-22 15:38:29 +0100

    refactor: extract actor attributes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 // For action titles, the first word has to start with a capital letter, followed by 0-3 other words, ignoring prepositions,
 // followed by a period. Support words with hyphens, non-capital first letter, and parentheses like '(Recharge 5-6)'.
 
+const attributesRegex =
+  /(?<attribute>[a-zA-z]{3})(\r\n|\r|\n)(?<base>\d+)\s+?\((?<mod>(\+|-)\d+)\)/g;
+
 // const actionTitleRegex =
 //   /^(([A-Z]\w+[ \-]?)(\s(of|and|the|from|in|at|on|with|to|by)\s)?(\w+ ?){0,3}(\([\w –\-\/]+\))?)\./;
 // const racialDetailsRegex =
@@ -13,7 +16,6 @@
 // const abilityNamesRegex = /\bstr\b|\bdex\b|\bcon\b|\bint\b|\bwis\b|\bcha\b/gi;
 // const abilitySavesRegex =
 //   /(?<name>\bstr\b|\bdex\b|\bcon\b|\bint\b|\bwis\b|\bcha\b) (?<modifier>[\+|-]\d+)/gi;
-const abilityValuesRegex = /(?<base>\d+)\s?\((?<modifier>[\+|\-|−|–]\d+)\)/g;
 // const skillsRegex =
 //   /(?<name>\bacrobatics\b|\barcana\b|\banimal handling\b|\bathletics\b|\bdeception\b|\bhistory\b|\binsight\b|\bintimidation\b|\binvestigation\b|\bmedicine\b|\bnature\b|\bperception\b|\bperformance\b|\bpersuasion\b|\breligion\b|\bsleight of hand\b|\bstealth\b|\bsurvival\b) (?<modifier>[\+|-]\d+)/gi;
 // const damageTypesRegex =
@@ -35,138 +37,6 @@ const abilityValuesRegex = /(?<base>\d+)\s?\((?<modifier>[\+|\-|−|–]\d+)\)/g
 // const damageRollsQuery =
 // "(?<={0})[\\s\\w\\d,]+\\((?<damageroll1>\\d+d\\d+)( \\+ (?<damagemod1>\\d+))?\\) (?<damagetype1>\\w+)(.+plus.+\\((?<damageroll2>\\d+d\\d+( \\+ (?<damagemod2>\\d+))?)\\) (?<damagetype2>\\w+))?";
 
-const KNOWN_SECTION_HEADERS = [
-  "actions",
-  "bonus actions",
-  "reactions",
-  "legendary actions",
-  "lair actions",
-  "regional effects",
-];
-
-const SECTION_IDS = {
-  0: "coreData",
-  1: "coreAttributes",
-  2: "abilities",
-  3: "mixedSection",
-  4: "legendaryResistances",
-  5: "features",
-  6: "actions",
-  7: "legendaryActions",
-};
-
-function findParagraphEnd(lines, statIdx) {
-  for (let j = statIdx + 1; j < lines.length; j++) {
-    const pLine = lines[j].trim().toLowerCase();
-    if (KNOWN_SECTION_HEADERS.includes(pLine)) {
-      // console.log("found end of paragraph at", j - 1);
-      return j - 1;
-    }
-  }
-  // console.log("no paragraph end found");
-  return lines.length - 1;
-}
-
-function findParagraphIndexes(lines) {
-  const pIndices = { start: undefined, end: undefined };
-
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i].trim();
-
-    const isNotSectionHeader =
-      !KNOWN_SECTION_HEADERS.includes(line.toLowerCase()) ||
-      line === "" ||
-      line === undefined;
-    if (isNotSectionHeader) {
-      continue;
-    }
-
-    // console.log("found start of paragraph at", i);
-
-    pIndices.start = i;
-    pIndices.end = findParagraphEnd(lines, i);
-    // console.log("paragraph starts", pIndices.start, "ends", pIndices.end);
-    return pIndices;
-  }
-
-  return pIndices;
-}
-
-function extractParagrah(lines, paragraphIndices) {
-  const start = paragraphIndices.start;
-  const end = paragraphIndices.end;
-  const length = end - start;
-  const paragraph = lines.splice(start, length + 1);
-  return paragraph;
-}
-
-function findParagraphs(lines) {
-  const paragraphs = [];
-
-  while (true) {
-    const pIndices = findParagraphIndexes(lines);
-    if (!pIndices.end) {
-      console.log("no paragraphs left");
-      break;
-    }
-    const p = extractParagrah(lines, pIndices);
-    const header = p.splice(0, 1)[0];
-    const paragraph = { header, description: p };
-    paragraphs.push(paragraph);
-    // console.log("paragraph", paragraph);
-    // console.log("lines", lines);
-  }
-  console.log("paragraph discovery finished");
-  return paragraphs;
-}
-
-function collectRawActionData(lines, sections) {
-  const paragraphs = findParagraphs(lines);
-  paragraphs.forEach((p) => {
-    const desc = p.description;
-    const sectionHeader = p.header.toLowerCase();
-    switch (sectionHeader) {
-      case "actions":
-      case "bonus actions":
-      case "reactions":
-        sections.actions = desc;
-        break;
-      case "legendary actions":
-        sections.legendaryActions = desc;
-        break;
-      case "lair actions":
-      case "regional effects":
-        sections.other = desc;
-        break;
-      default:
-        console.log("unknown header", sectionHeader);
-        break;
-    }
-  });
-}
-
-function collectRemainingRawActorData(lines, sections) {
-  let sectionIndex = 0;
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i].trim();
-    // console.log("lines.length", lines.length);
-    // console.log("line " + i, line);
-
-    const newSection = line.length <= 0 && i != lines.length;
-    if (newSection) {
-      sectionIndex += 1;
-      continue;
-    }
-    // console.log("sectionIds", SECTION_IDS);
-    // console.log("sectionIndex", sectionIndex);
-
-    const sectionKey = SECTION_IDS[sectionIndex] ?? "other";
-    // console.log("sectionKey", sectionKey);
-    sections[sectionKey].push(line);
-  }
-  console.log("sections", sections);
-}
-
 function createActorSheet(actorData) {
   const sections = {
     coreData: [],
@@ -180,115 +50,27 @@ function createActorSheet(actorData) {
     other: [],
   };
 
-  const lines = actorData.trim().split(/\n/g);
-  console.log("user input", lines);
-  collectRawActionData(lines, sections);
-  collectRemainingRawActorData(lines, sections);
-  const abilities = gatherAbilityData(sections.abilities);
+  // todo
 
-  // for (const [key, val] of sections) {
-  //   console.log(key, ": ", val);
-  // }
-
-  /**
-   * OTHER VALUES TO BE SET
-   * types = "npc" => currently only NPC possilbe
-   * initative    => common.attributes.init
-   * blindsight   => common.creature.attributes.senses.blindsight
-   * tremorsense  => common.creature.attributes.senses.tremorsense
-   * truesight    => common.creature.attributes.senses.truesight
-   */
-
-  /**
-   * OTHER VALUES TO =>>MAY<<= SET
-   * description                    => common.details
-   * currency                       => common.currency
-   * skills (acrobatics and so on)  => common.creature.skills
-   * spells                         => common.creature.spells
-   * spell DC                       => common.creature.bonuses.spell.dc
-   * spellcasting attribute         => common.creature.attributes.spellcasting
-   * swarm                          => common.npc.details.type.swarm
-   * NPC type                       => common.npc.details.type
-   * Book source                    => common.npm.details.source
-   * Vehicle                        => common.vehicle
-   */
-
-  // Goblin               => name
-  // Small                => common.traits.size
-  // humanoid (goblinoid) => common.creature.details.race
-  // neutral evil         => common.creature.details.alignment
-
-  // Armor Class 15 (Leather Armor, Shield) => common.attributes.ac
-
-  // Hit Points 7 (2d6) => common.attributes.hp
-  // Speed 30 ft.       => common.attributes.movement
-
-  /**
-   * common.abilities
-   */
-  // STR      => common.abilities[str]
-  // 8        => common.abilities[str].value
-  // (-1)     => common.abilities[str].bonuses.check & common.abilities[str].bonuses.save
-  // DEX
-  // 14 (+2)
-  // CON
-  // 10 (+0)
-  // INT
-  // 10 (+0)
-  // WIS
-  // 8 (-1)
-  // CHA
-  // 8 (-1)
-
-  // Skills Stealth +6          => common.creature.skills.ste
-  // Senses Darkvision 60 ft.   => common.creature.attributes.senses.darkvision
-  // Passive Perception 9       => common.creature.skills.per.bonuses.passive
-  // Languages Common, Goblin   => common.creature.traits.languages
-  // Challenge 1/4              => common.npc.cr
-  // (50 XP)                    => common.npc.xp.value
-  // Proficiency Bonus +2       => ???
-
-  /**
-   * Features => template.json => feat
-   */
-  // Nimble Escape. The goblin can take the Disengage or Hide action as a bonus action on each of its turns.
-
-  /**
-   * Actions => ???
-   */
-  // Scimitar. Melee Weapon Attack: +4 to hit, reach 5 ft., one target. Hit: 5 (1d6 + 2) slashing damage.
-  // Shortbow. Ranged Weapon Attack: +4 to hit, range 80/320 ft., one target. Hit: 5 (1d6 + 2) piercing damage.
+  function gatherAttributes() {
+    const attributes = {};
+    let match;
+    while ((match = attributesRegex.exec(actorData)) != null) {
+      attributes[match.groups.attribute.toLocaleLowerCase()] = {
+        base: match.groups.base,
+        mod: match.groups.mod,
+      };
+    }
+    return attributes;
+  }
+  const attributes = gatherAttributes();
+  console.log("attributes", attributes);
 
   // todo
   // await Actor.create({
   //   name: "Test NPC",
   //   type: "npc",
   // });
-}
-
-function gatherAbilityData(rawAbilities) {
-  // todo
-  const abilities = {};
-  let abilityName = "";
-  const isAbilityName = /^[A-Za-z]+$/;
-
-  rawAbilities.forEach((a) => {
-    if (isAbilityName.test(a)) {
-      abilityName = a.trim().toLowerCase();
-    } else {
-      // todo set value abilityValuesRegex
-      const results = abilityValuesRegex.exec(a);
-      if (results) {
-        const val = results.groups;
-        console.log(val);
-        abilities[abilityName] = val;
-        abilityName = "";
-      }
-    }
-  });
-
-  console.log(abilities);
-  return abilities;
 }
 
 // used for item, spell, class, monster feature, racial feature
