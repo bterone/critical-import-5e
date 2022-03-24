c18fdb41aeb3c26d9cfd228b7feb66c8628c0996
PragmaticBeaver <dominik.blatzheim@mailbox.org>
2022-03-24 20:13:42 +0100

    chore: project restructure and code encapsulation

---
+++ src/scripts/dialog.js
@@ -0,0 +1,49 @@
+export function openImportDialog(title, callback) {
+  /**
+   * HTMLElement.outerHTML does't work for input.value!
+   * => because of this, a string representation is necessary
+   */
+  const content =
+    `
+       <div>
+        <textarea id="importDialog` +
+    title +
+    `" wrap="hard" cols="1" placeholder="Usage:
+          - Paste the full ` +
+    title +
+    `statblock text, formatted following the WotC formular, into this box.
+          - If copy-paste errors occure, a fix inside this box may be done."></textarea>
+       </div>
+     `;
+
+  let d = new Dialog({
+    title: "Critical Import 5e - Import " + title,
+    content,
+    buttons: {
+      done: {
+        icon: '<i class="fas fa-file-download"></i>',
+        label: "Import",
+        callback: callback,
+      },
+    },
+    default: "close",
+    render: (html) =>
+      console.log("onRender - Register interactivity in the rendered dialog"),
+    close: (html) => {
+      console.log("closed Import " + title + " dialog");
+    },
+  });
+  d.render(true);
+}
+
+export function createImportButton(label, callback) {
+  const icon = document.createElement("i");
+  icon.classList = "fas fa-file-download";
+
+  const btn = document.createElement("button");
+  btn.classList = "dndbeyondSync-menu-btn";
+  btn.innerText = label;
+  btn.onclick = callback;
+  btn.appendChild(icon);
+  return btn;
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,66 @@
+import { openImportDialog, createImportButton } from "./dialog.js";
+import { importActor } from "./import-actor.js";
+
+// function getDialogId(label) {
+//   return "importDialog-" + label + "-input";
+// }
+
+Hooks.on("ready", () => {
+  console.log("critical-import-5e | starting ...");
+  console.log("TEST2");
+});
+
+const DIALOG_ID = "importDialog";
+// monster, NPC
+Hooks.on("renderActorDirectory", (args) => {
+  const footer = args.element[0].getElementsByTagName("footer")[0];
+  const label = "Actor ";
+  footer.appendChild(
+    createImportButton("Import " + label, () => {
+      openImportDialog(label, async () => {
+        const actorData = document.getElementById(DIALOG_ID + label).value;
+        await importActor(actorData);
+      });
+    })
+  );
+});
+
+// item, spell, class, monster feature, racial feature
+Hooks.on("renderItemDirectory", (args) => {
+  const footer = args.element[0].getElementsByTagName("footer")[0];
+  const labelItem = "Item ";
+  footer.appendChild(
+    createImportButton("Import " + labelItem, () => {
+      openImportDialog(labelItem, () => {
+        console.log(labelItem);
+      });
+    })
+  );
+
+  const labelSpell = "Spell ";
+  footer.appendChild(
+    createImportButton("Import " + labelSpell, () => {
+      openImportDialog(labelSpell, () => {
+        console.log(labelSpell);
+      });
+    })
+  );
+
+  const labelClass = "Class ";
+  footer.appendChild(
+    createImportButton("Import " + labelClass, () => {
+      openImportDialog(labelClass, () => {
+        console.log(labelClass);
+      });
+    })
+  );
+
+  const labelFeature = "Feature ";
+  footer.appendChild(
+    createImportButton("Import " + labelFeature, () => {
+      openImportDialog(labelFeature, () => {
+        console.log(labelFeature);
+      });
+    })
+  );
+});
