d1266bafa272e0fa80fb16d9e749fa4baf020f9c
Beaver <8586904+PragmaticBeaver@users.noreply.github.com>
2022-04-13 20:11:34 +0200

    refactor: extract raw spell data

--- src/scripts/import-spell/gather-spell-data.js
+++ src/scripts/import-spell/gather-spell-data.js
@@ -26,5 +26,45 @@ const logger = new Logger("gather-spell-data.js");
 
 export function gatherSpellData(importedSpellData) {
   // todo
+  // gather D&D Beyond
+  // gather WotC style (Grimhollow PDF's for example)
+
   logger.logConsole("importedSpellData", importedSpellData);
+  const validHeaders = [
+    "level",
+    "casting time",
+    "range/area",
+    "components",
+    "duration",
+    "school",
+    "attack/save",
+    "damage/effect",
+  ];
+
+  const inputPortions = importedSpellData.trim().split(/\n/g);
+
+  const rawSpellDto = { name: inputPortions.splice(0, 1)[0], other: [] };
+
+  let currentHeader = undefined;
+  for (const portion of inputPortions) {
+    const value = portion.toLocaleLowerCase();
+    if (!value) {
+      continue;
+    }
+
+    if (validHeaders.includes(value)) {
+      // is header
+      currentHeader = value;
+      logger.logConsole("set currentHeader", currentHeader);
+    } else if (currentHeader !== undefined) {
+      // is value
+      rawSpellDto[currentHeader] = portion;
+      currentHeader = undefined;
+    } else {
+      // is value without known header
+      rawSpellDto.other.push(portion);
+    }
+  }
+
+  logger.logConsole("rawSpellDto", rawSpellDto);
 }
